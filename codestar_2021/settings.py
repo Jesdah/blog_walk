686fc41d27a35ca1533a6402b61c63bd63b340c6
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-07-19 15:38:36 +0000

    deployment

--- codestar_2021/settings.py
+++ codestar_2021/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [["codestar_2021.herokuapp.com", "BLOG_WALK"]]
+ALLOWED_HOSTS = [["codestar_2021.herokuapp.com", "blog-walk-jesper"]]
 
 
 # Application definition
